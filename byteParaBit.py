442ffa6f4cc6956a076bd3347cf6b58d0a9382e6
unknown <brenolimac17@gmail.com>
2023-04-20 09:31:08 -0300

    Byte para Bit concluído

--- byteParaBit.py
+++ byteParaBit.py
@@ -2,12 +2,12 @@ def converterStringParaFloat(value):
     print('Valor convertido de str para float')
     return float(value)
 
-def bitParaByte(valorASerConvertido):
-    print('Valor convertido de bit para byte')
-    bytesCalculado = valorASerConvertido / 8
-    return bytesCalculado
+def byteParaBit(valorASerConvertido):
+    print('Valor convertido de byte para bit')
+    bitsCalculado = valorASerConvertido * 8
+    return bitsCalculado
 
 print('Insira o valor a ser convertido')
 entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
-valorConvertido = bitParaByte(entradaDoTecladoValorASerConvertido)
+valorConvertido = byteParaBit(entradaDoTecladoValorASerConvertido)
 print(valorConvertido)
